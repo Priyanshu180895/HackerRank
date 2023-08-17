8c314d9e51fa0174eb2d52eef7214ad7efce953a
Priyanshu180895 <142256739+Priyanshu180895@users.noreply.github.com>
2023-08-18 00:43:17 +0530

    adding subarray division2

---
+++ week3/subarray_division2.py
@@ -0,0 +1,24 @@
+def birthdays(s,d,m):
+    i = 0
+    j = m
+    count = 0
+
+    while j <= len(s):
+        if sum(s[i:j]) == d:
+            count += 1
+        i += 1
+        j += 1
+    return count
+
+if __name__ == "__main__":
+    n = int(input("No. of elements in a array: ").strip())
+    s = list(map(int,input("Enter elements of array: ").rstrip().split()))
+
+    birthday_days_month = input("Enter b'day day and month: ").rstrip().split()
+
+    d = int(birthday_days_month[0])
+    m = int(birthday_days_month[1])
+
+    result = birthdays(s, d,m)
+
+    print(result)
